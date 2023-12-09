d6a69e8b5a6d3c0a6e7827c1c88ba8fb75581d4f
James Gore <83005220+thejamesgore@users.noreply.github.com>
2023-12-09 23:46:38 +0000

    Update background.js

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
   if (changeInfo.status === 'loading' && tab.url.includes('youtube.com')) {
-    const baseURL = `192.168.30.31:38912` // Change this to your base url and port for your self hosted or public youtube alternative. E.g Invidious.
+    const baseURL = `https://yewtu.be` // Change this to your base url and port for your self hosted or public youtube alternative. E.g Invidious.
 
     const parts = tab.url.split('.com')
     if (parts.length > 1) {
